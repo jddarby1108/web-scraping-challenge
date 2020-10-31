aef65b18afdb1583c7ec50e6843b90d5880db821
jddarby <67566019+jddarby1108@users.noreply.github.com>
2020-10-30 23:05:40 -0500

    adding scrape code to test from craigslist

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect
+from flask_pymongo import PyMongo
+
+
+# From the separate python file in this directory, we'll import the code that is used to scrape mars data
+import mission_to_mars
+
+app = Flask(__name__)
+
+# Use flask_pymongo to set up mongo connection
+app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
+mongo = PyMongo(app)
+
+# identify the collection and drop any existing data for this demonstration
+listings = mongo.db.listings
+listings.drop()
+
+# Render the index.html page with any mars data listings in our database. 
+# If there are no listings, the table will be empty.
+@app.route("/")
+def index():
+    listing_results = listings.find()
+    return render_template("index.html", listing_results=listing_results)
+
+# This route will trigger the webscraping, but it will then send us back to the index route to render the results
+@app.route("/scrape")
+def scraper():
+
+    # scrape_craigslist.scrape() is a custom function that we've defined in the scrape_craigslist.py file within this directory
+    listings_data = mission_to_mars.scrape
+    listings.insert_many(listings_data)
+    
+    # Use Flask's redirect function to send us to a different route once this task has completed.
+    return redirect("/")
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ mission_to_mars.py
@@ -0,0 +1,144 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[184]:
+
+
+# Import dependencies
+import pandas as pd
+from splinter import Browser
+from splinter.exceptions import ElementDoesNotExist
+from bs4 import BeautifulSoup
+import requests
+
+
+# In[185]:
+
+
+# Chromedriver
+executable_path = {'executable_path': 'chromedriver.exe'}
+browser = Browser('chrome', **executable_path, headless=False)
+
+
+# In[186]:
+
+
+# website to scrape
+url = 'https://mars.nasa.gov/news/'
+browser.visit(url)
+
+
+# In[187]:
+
+
+# Splinter can capture a page's underlying html and use pass it to BeautifulSoup to allow us to scrape the content
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+# print(soup.prettify())
+# Using BS, we can execute standard functions to capture the page's content
+
+news_title=soup.find('li', attrs={'class': 'slide'}).find('h3').text
+print(news_title)
+
+news_p = soup.find('div', class_='article_teaser_body').text
+print(news_p)      
+
+
+# In[188]:
+
+
+# website to scrape
+img_url = 'https://www.jpl.nasa.gov/spaceimages/?search=&category=Mars'
+browser.visit(img_url)
+
+# Splinter can capture a page's underlying html and use pass it to BeautifulSoup to allow us to scrape the content
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+
+featured_image_url = soup.find('li', class_='slide').find('a', class_='fancybox')['data-fancybox-href'] 
+print(featured_image_url)
+# print(soup.prettify())
+
+   
+
+
+# In[190]:
+
+
+# Scrape Mars hemisphere image url and title
+hemisphere_facts_url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
+browser.visit(hemisphere_facts_url)
+
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+
+# capture all of the items in the item results
+titles = soup.find_all('div', class_='item')
+
+# find the list of categories in the left pane
+lists = soup.find('div', class_='description')
+
+image_urls = {}
+try:
+    for title in titles:
+
+            for list in lists:
+
+                # find the list of categories in the left pane
+                lists = soup.find('div', class_='description')
+
+                # Capture the category names and their URLs
+                list = lists.find('h3').text
+                image_urls[list] = lists.find('a')['href']
+    # image_url I get one whoop! I do not know why it will not loop
+    print(list + " "+ hemisphere_facts_url + image_urls[list])
+
+except AttributeError as e:
+    print(e)
+
+                  
+
+
+
+
+# In[191]:
+
+
+#     # Dictionary to be inserted into MongoDB
+    
+hemisphere_image_urls = [
+    {"Cerberus Hemisphere": list, "hemi_urls": hemisphere_facts_url + image_urls[list]},
+]
+hemisphere_image_urls
+
+
+# In[203]:
+
+
+# website to scrape
+table_url = 'https://space-facts.com/mars/'
+browser.visit(table_url)
+
+# Capture Table
+html = browser.html
+soup = BeautifulSoup(html, 'html.parser')
+
+table = pd.read_html(table_url) 
+print(table[0])
+# print(soup.prettify())
+
+
+# In[204]:
+
+
+df = table[0]
+df.columns = ['Description', 'Data']
+df.set_index('Description', inplace=True)
+df.head()
+
+
+# In[ ]:
+
+
+
+
